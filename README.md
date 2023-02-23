d90c3ec19a1dd997674f95c9600175521dba856b
Lunesombre <romain.touchet@gmail.com>
2023-02-23 16:31:44 +0100

    Enoncé

---
+++ README.md
@@ -0,0 +1,8 @@
+# Atelier PHP - Liste de produits
+
+- Prendre le fichier index.php
+- Réaliser dans ce fichier une page qui liste les produits du tableau $products
+- Vous pouvez utiliser une boucle foreach
+- Toutes les colonnes doivent apparaître d'une façon ou d'une autre
+- Vous êtes libres sur l'apparence de la page
+- Il n'y a QUE la page index.php à faire, donc la liste des produits
---
+++ index.php
@@ -0,0 +1,183 @@
+<?php
+$products = [
+    [
+        'id' => 1,
+        'name' => 'oQooMUTq',
+        'priceHT' => 225.63,
+        'promo' => false
+    ],
+    [
+        'id' => 2,
+        'name' => 'qPsqI',
+        'priceHT' => 395.94,
+        'promo' => false
+    ],
+    [
+        'id' => 3,
+        'name' => 'prBPvJm',
+        'priceHT' => 179.73,
+        'promo' => false
+    ],
+    [
+        'id' => 4,
+        'name' => 'KClnXU',
+        'priceHT' => 744.21,
+        'promo' => false
+    ],
+    [
+        'id' => 5,
+        'name' => 'wDkFHqocMyqWTbj',
+        'priceHT' => 383.95,
+        'promo' => false
+    ],
+    [
+        'id' => 6,
+        'name' => 'ZOlqRbrjziyKbhjB',
+        'priceHT' => 248.11,
+        'promo' => false
+    ],
+    [
+        'id' => 7,
+        'name' => 'TBASHgIz',
+        'priceHT' => 826.33,
+        'promo' => true
+    ],
+    [
+        'id' => 8,
+        'name' => 'vAMsclORHatUdCmBK',
+        'priceHT' => 456.13,
+        'promo' => false
+    ],
+    [
+        'id' => 9,
+        'name' => 'gySFYsWDCLTYPtUz',
+        'priceHT' => 197.22,
+        'promo' => false
+    ],
+    [
+        'id' => 10,
+        'name' => 'WAEXaFslNNDmyxr',
+        'priceHT' => 725.69,
+        'promo' => false
+    ],
+    [
+        'id' => 11,
+        'name' => 'ZFKWWRaUmEhmTmGj',
+        'priceHT' => 700.56,
+        'promo' => true
+    ],
+    [
+        'id' => 12,
+        'name' => 'xlNTrZxPzeVBbxXAxNVH',
+        'priceHT' => 749.08,
+        'promo' => false
+    ],
+    [
+        'id' => 13,
+        'name' => 'YNIwNpBTd',
+        'priceHT' => 676.57,
+        'promo' => false
+    ],
+    [
+        'id' => 14,
+        'name' => 'fMfLuGYjXRQbwgIwqhc',
+        'priceHT' => 718.1,
+        'promo' => false
+    ],
+    [
+        'id' => 15,
+        'name' => 'yBmRty',
+        'priceHT' => 130.22,
+        'promo' => true
+    ],
+    [
+        'id' => 16,
+        'name' => 'KOdbPaeBqrIYatAP',
+        'priceHT' => 170.97,
+        'promo' => false
+    ],
+    [
+        'id' => 17,
+        'name' => 'fFDFcjvWHO',
+        'priceHT' => 424.49,
+        'promo' => false
+    ],
+    [
+        'id' => 18,
+        'name' => 'MxPiGWKtxdvT',
+        'priceHT' => 469.84,
+        'promo' => false
+    ],
+    [
+        'id' => 19,
+        'name' => 'EKQHXDqbcXOKTqn',
+        'priceHT' => 210.31,
+        'promo' => true
+    ],
+    [
+        'id' => 20,
+        'name' => 'dCNkYMbUVyierTtJhDQ',
+        'priceHT' => 711.42,
+        'promo' => false
+    ],
+    [
+        'id' => 21,
+        'name' => 'pFCkcqzbfY',
+        'priceHT' => 758.76,
+        'promo' => false
+    ],
+    [
+        'id' => 22,
+        'name' => 'vPOUTDlINKWGlGpo',
+        'priceHT' => 389.1,
+        'promo' => false
+    ],
+    [
+        'id' => 23,
+        'name' => 'rSKDPmLXQxOkVnYvnl',
+        'priceHT' => 739.27,
+        'promo' => true
+    ],
+    [
+        'id' => 24,
+        'name' => 'fXgCXSF',
+        'priceHT' => 262.3,
+        'promo' => false
+    ],
+    [
+        'id' => 25,
+        'name' => 'xzhNfwgYG',
+        'priceHT' => 807.83,
+        'promo' => false
+    ],
+    [
+        'id' => 26,
+        'name' => 'MSjQEUiaLcvuaPqD',
+        'priceHT' => 525.78,
+        'promo' => false
+    ],
+    [
+        'id' => 27,
+        'name' => 'rPoGffmylTpqxW',
+        'priceHT' => 188.37,
+        'promo' => true
+    ],
+    [
+        'id' => 28,
+        'name' => 'VPZwaidfrbUbOzdIv',
+        'priceHT' => 407.58,
+        'promo' => false
+    ],
+    [
+        'id' => 29,
+        'name' => 'ImuUwFclCPCK',
+        'priceHT' => 374.67,
+        'promo' => false
+    ],
+    [
+        'id' => 30,
+        'name' => 'ocxXehz',
+        'priceHT' => 776.5,
+        'promo' => true
+    ],
+];
